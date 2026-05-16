08619429a65aa8e698f3f5ecd354faf345846998
teo7991 <116755717+teo7991@users.noreply.github.com>
2026-05-16 06:04:46 +0400

    feat: initial items app with Angular forms and list rendering

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,49 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal,  } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+// import { RouterOutlet } from '@angular/router';
+
+interface Item {
+  id: number;
+  title: string;
+  sold: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('mymarket-clone');
+
+  itemName: string = '';
+
+  items: Item[] = [
+    {
+      id: 1,
+      title: 'Car',
+      sold: false,
+    },
+    {
+      id: 2,
+      title: 'Laptop',
+      sold: true,
+    }
+  ];
+
+  addItem(): void{
+    const newItem: Item = {
+      id: Date.now(),
+      title: this.itemName,
+      sold: false,
+    };
+
+    this.items.push(newItem);
+
+    this.itemName = '';
+  }
 }
